44e69ce586251d96ad174841d0603f431668517b
narendra.singh5@dellteam.com <narendra.singh5@dellteam.com>
2022-04-25 10:51:25 +0100

    test commit

--- basic-lwc-app-master/src/client/modules/my/students/students.js
+++ basic-lwc-app-master/src/client/modules/my/students/students.js
@@ -2,6 +2,7 @@ import { LightningElement, track } from 'lwc';
 
 export default class Students extends LightningElement {
     @track students = [];
+    @track knowledges = [];
 
     getStudents() {
         //using javascrip native fetch method to get data from server
@@ -19,5 +20,23 @@ export default class Students extends LightningElement {
     //getter method to check if students array have value
     get isStudentsAvailable() {
         return this.students.length > 0;
+    }
+
+    getKnowledges() {
+        //using javascrip native fetch method to get data from server
+        fetch('/knowledges')
+            .then(res => {
+                res.json().then(data => {
+                    this.knowledges = data;
+                    console.log(data);
+                });
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    //getter method to check if students array have value
+    get isKnowledgesAvailable() {
+        return this.knowledges.length > 0;
     }
 }
--- basic-lwc-app-master/src/server/index.js
+++ basic-lwc-app-master/src/server/index.js
@@ -10,16 +10,49 @@ module.exports = app => {
             },
             {
                 name: 'Elizabeth Johnson',
-                age: 17,
+                age: 18,
                 id: 2
             },
             {
                 name: 'David Wilson',
-                age: 17,
+                age: 19,
                 id: 3
             }
         ];
         //next line will send response in JSON format
         res.json(students);
+    });
+
+    app.get('/knowledges', (req, res) => {
+        var knowledges = [
+            {
+                title: 'Likes / Dislikes Test article',
+                summary: 'Like/DisLike Test Articles for Knowledge Articles'
+            },
+            {
+                title: 'Testing heroku flow 1',
+                summary: 'Testing heroku flow 1'
+            },
+            {
+                title: 'How to hide the field from Page Layout',
+                summary:
+                    'How to hide the field from Page Layout and component so that field will not be visible to customers.'
+            },
+            {
+                title: 'How to add Video Files in Knowledge Article',
+                summary:
+                    'The whole point of embedding Youtube videos in another, is that iFrames are used to display their player interface (and run its supporting code). This is done by accessing the embedable player/code by using an iFrame.'
+            },
+            {
+                title: 'How to Reset Password',
+                summary: 'Process to change the Password'
+            },
+            {
+                title: 'My new Knowledge Article E',
+                summary: 'For demo, showing the new page with test formatting'
+            }
+        ];
+
+        res.json(knowledges);
     });
 };
